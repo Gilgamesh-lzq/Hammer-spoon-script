5196826032a786d944d8ef74ed575ac8edc3f305
Gilgamesh-lzq <1794896442@qq.com>
2022-07-30 17:51:13 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,9 +7,9 @@ require("markdown")
 -- 所有提示语句的显示样式
 hs.alert.defaultStyle = {
   strokeWidth  = 5, -- 边框宽度
-  strokeColor = { white = 0, alpha = 1 }, -- 边框颜色
+  strokeColor = { white = 0, alpha = 0.5 }, -- 边框颜色
   fillColor   = { white = 1, alpha = 0.7 }, -- 背景颜色
-  textColor = { white = 0, alpha = 1 }, -- 文字颜色
+  textColor = { black=0.345, alpha=1 }, -- 文字颜色
   textFont  = "PingFangSC-Medium", -- ".AppleSystemUIFont"
   textSize  = 42,
   radius = 25,
@@ -54,6 +54,21 @@ end)
 
 -- 鼠标下键 前进
 -- 前进很少使用，已弃用此功能。或许下键设定为mission_control更合适
-hs.hotkey.bind({}, "f17", function() 
-  hs.eventtap.keyStroke('cmd', ']', 0)
+-- hs.hotkey.bind({}, "f17", function() 
+--   hs.eventtap.keyStroke('cmd', ']', 0)
+-- end)
+
+-- 便捷控制 Terminal 打开关闭. Vscode无窗口时自动关闭
+hs.hotkey.bind({}, "f4", function() 
+  terminal = hs.application.get("终端")
+  if not terminal then
+    hs.application.launchOrFocus("Terminal.app")
+  else
+    terminal:kill9()
+  end
+
+  vscode = hs.application.get("Code")
+  if vscode and not next(vscode:allWindows()) then
+    vscode:kill9()
+  end
 end)
